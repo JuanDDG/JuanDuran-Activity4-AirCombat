c1c9948e0466c8773520c734bab1d07876d358f6
JuanDDG <neo.billonario@hotmail.com>
2018-10-06 14:55:53 -0500

    Primera versión del proyecto

---
+++ Assets/Scripts/MissileMotion.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissileMotion : MonoBehaviour
+{
+
+    public float motionSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        this.transform.Translate(Vector3.forward * motionSpeed);
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Colisión entre dos colliders");
+
+        //Destrucción del GameObject
+        GameObject.Destroy(this.gameObject);
+
+        //Destrucción del enemigo
+        GameObject.Destroy(collision.collider.gameObject);
+    }
+}
---
+++ Assets/Scripts/PlaneMotion.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneMotion : MonoBehaviour
+{
+
+    public float motionSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        this.transform.Translate(Vector3.forward);
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.Translate(Vector3.left * motionSpeed);
+
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.Translate(Vector3.right * motionSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            this.transform.Translate(Vector3.up * motionSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            this.transform.Translate(Vector3.down * motionSpeed);
+        }
+
+
+        //Spacebar
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Barra Espaciadora presionada");
+
+            //Creamos un GameObject a partir de un prefab (planos a partir de un edificio)
+            GameObject newMissile = GameObject.Instantiate(Resources.Load("Bullets/Missile 2") as GameObject);
+
+            // Buscamos el GameObject del pivote de disparo (ShootPivot)
+            GameObject shootPivot = GameObject.Find("CombatPlane/ShootPivot");
+
+            //Hago el misil hijo de ShootPivot
+            newMissile.transform.SetParent(shootPivot.transform);
+
+            //Movemos el misil a la posición del pivote de disparo
+            newMissile.transform.localPosition = Vector3.zero;
+
+            //Sacar el misil del pivote para que no se mueva con el avión
+            newMissile.transform.SetParent(null);
+
+        }
+    }
+}
